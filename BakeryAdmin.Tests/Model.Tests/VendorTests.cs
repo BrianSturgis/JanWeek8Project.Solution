9ed3918c36689af35e740c8bab60ddac0038a47d
Brian Sturgis <sturujisu@gmail.com>
2021-03-05 18:29:03 -0800

    a good pass for VendorConstructor_CreatesInstanceOfVendor_Vendor

--- BakeryAdmin.Tests/Model.Tests/VendorTests.cs
+++ BakeryAdmin.Tests/Model.Tests/VendorTests.cs
@@ -15,5 +15,34 @@ namespace BakeryAdmin.Tests
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
 
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
+
+      //Act
+      string result = newVendor.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void GetId_ReturnsVendorId_Int()
+    {
+      //Arrange
+      string name = "Test Vendor";
+      Category newCategory = new Category(name);
+
+      //Act
+      int result = newCategory.Id;
+
+      //Assert
+      Assert.AreEqual(1, result);
+    }
+
+
   }
 }
