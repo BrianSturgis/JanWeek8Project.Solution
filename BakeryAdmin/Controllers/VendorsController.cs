f999340dd9948722913e927abbf4652fc7e93b86
Brian Sturgis <sturujisu@gmail.com>
2021-03-05 19:46:16 -0800

    complete vendor order collection functionality

---
+++ BakeryAdmin/Controllers/VendorsController.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using BakeryAdmin.Models;
+
+namespace BakeryAdmin.Controllers
+{
+  public class VendorsController : Controller
+  {
+  [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+    [HttpGet("/categories/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName)
+    {
+      Vendor newVendor = new Vendor(vendorName);
+      return RedirectToAction("Index");
+    }
+    [HttpGet("/vendors/{id}")]
+  public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("Vendor", selectedVendor);
+      model.Add("Orders", vendorOrders);
+      return View(model);
+    }
+  }
+}
